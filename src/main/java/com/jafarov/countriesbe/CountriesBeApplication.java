9f9aa2834f247352874ae86a13edaa817723ae8a
tahirceferov <tahirceferovvv@gmail.com>
2024-03-27 08:36:01 +0400

    Initial commit

---
+++ src/main/java/com/jafarov/countriesbe/CountriesBeApplication.java
@@ -0,0 +1,13 @@
+package com.jafarov.countriesbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountriesBeApplication.class, args);
+    }
+
+}
