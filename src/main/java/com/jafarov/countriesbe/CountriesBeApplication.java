01ef26f81473fbc26ff9ac5251fbfc2cd873410c
tahirceferov <tahirceferovvv@gmail.com>
2024-03-27 16:01:04 +0400

    second

--- src/main/java/com/jafarov/countriesbe/CountriesBeApplication.java
+++ src/main/java/com/jafarov/countriesbe/CountriesBeApplication.java
@@ -1,12 +1,15 @@
 package com.jafarov.countriesbe;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CountriesBeApplication {
 
+
     public static void main(String[] args) {
+
         SpringApplication.run(CountriesBeApplication.class, args);
     }
 
